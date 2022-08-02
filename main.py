de1d52dd208d4619fa8bf3d929e57b17210a0839
cat_or_not <41955154+catornot@users.noreply.github.com>
2022-08-01 23:00:54 -0400

    adding some trol stuff

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
+from trol import Trol
 
 with open( "secret.txt", "r" ) as file:
     secret = file.read()
 
+# prefix 
 bot = commands.Bot(command_prefix='?')
 
-@bot.command()
-async def test(ctx):
-    await ctx.send( "test" )
+# adding cogs
+bot.add_cog(Trol(bot))
 
-
+# token and run
 bot.run( secret )
